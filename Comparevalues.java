1a2f406f7e5fdd312976e3a085539d36c5c11c4c
mohitp005 <101631564+mohitp005@users.noreply.github.com>
2022-03-15 16:47:21 +0530

    first uploadation

---
+++ Comparevalues.java
@@ -0,0 +1,18 @@
+package Basiccommands;
+//wap to compare two numbers 
+
+public class Comparevalues {
+
+	public static void main(String[] args) {
+      
+		int y=10;
+		  ++y;
+		System.out.println(y);  //postfix increment operator  //11  11 
+		
+		 
+		 
+ 		
+
+	}
+
+}
